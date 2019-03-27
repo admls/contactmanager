8d5f6c246a81d730cd36c6cae5f71a339b7be944
admls <adam.the.sherwood@gmail.com>
2019-03-27 19:02:03 +0000

    Updates

---
+++ precache-manifest.1046469c0ae2188631ef35e3c8f86e51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  },
+  {
+    "revision": "97ef112c2a83eed51777",
+    "url": "/contactmanager/static/js/main.08520f93.chunk.js"
+  },
+  {
+    "revision": "a4a0f5941b7962d57368",
+    "url": "/contactmanager/static/js/2.88b5c646.chunk.js"
+  },
+  {
+    "revision": "97ef112c2a83eed51777",
+    "url": "/contactmanager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "a4a0f5941b7962d57368",
+    "url": "/contactmanager/static/css/2.b0c6f6ca.chunk.css"
+  },
+  {
+    "revision": "9c9431ded65f9e77a804130c24dd5cf8",
+    "url": "/contactmanager/index.html"
+  }
+];
